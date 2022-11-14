d9a6e8076ea6e1c446871d12a458ff7ed1ee262e
vagashkov <vitali.agashkov@gmail.com>
2022-11-14 20:53:06 +0300

    Update home page tests

--- src/blog/tests.py
+++ src/blog/tests.py
@@ -1,15 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Post
 
 
 class HomePageTest(TestCase):
+    def setUp(self):
+        Post.objects.create(caption="Test post", text="Test post text")
+
     def test_open_home_page(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blogs/home.html')
 
 
 class AboutPageTest(TestCase):
     def test_open_about_page(self):
-        response = self.client.get('/about/')
+        response = self.client.get(reverse('about'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blogs/about.html')
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(caption="Test post", text="Test post text")
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'Test post text')
