d7272041b75e87071e382e8a8db936d8436bd96b
vagashkov <vitali.agashkov@gmail.com>
2022-11-14 20:42:04 +0300

    Add posts list on home page

--- src/blog/models.py
+++ src/blog/models.py
@@ -5,3 +5,5 @@ class Post(models.Model):
     caption = models.TextField()
     text = models.TextField()
 
+    def __str__(self):
+        return self.caption[:50]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import Post
 
 
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = 'blogs/home.html'
+    context_object_name = 'all_posts'
 
 
 class AboutPageView(TemplateView):
