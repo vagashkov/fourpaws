7d3d713441689cdfc7eb1c3e3951eef27e7b54f0
vagashkov <vitali.agashkov@gmail.com>
2022-11-22 20:28:22 +0300

    Add custom user model

--- src/blog/models.py
+++ src/blog/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200, default="No title")
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, default=1)
+    author = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, default=1)
     text = models.TextField()
 
     def __str__(self):
--- src/config/settings.py
+++ src/config/settings.py
@@ -129,5 +129,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = "post_list"
 LOGOUT_REDIRECT_URL = "post_list"
